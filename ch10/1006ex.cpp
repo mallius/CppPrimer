6f8a4ffad380803d1921dd729b0ffa10247e223c
mallius <mallius@qq.com>
2018-05-27 16:03:39 +0800

    .

---
+++ ch10/1006ex.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <list>
+#include <map>
+#include <vector>
+
+using std::string;
+using std::list;
+using std::map;
+using std::vector;
+int main(void)
+{
+	map<vector<int>::iterator, int> a;
+	map<list<int>::iterator, int> b;
+	//map<pair<int,string>, int> c;
+	return 0;
+}
