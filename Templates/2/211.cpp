067f07ac251af4a8ff95cac2a000b23cad1f23cd
mallius <mallius@qq.com>
2021-03-13 20:39:22 +0800

    templates

---
+++ Templates/2/211.cpp
@@ -0,0 +1,90 @@
+﻿/***
+第三节类模板中的友元
+(1)友元类：
+让某个类B称为另外一个类A的友元类，这样的话，类B就可以在其成员函数中访问类A的所有成员，
+而不管这些成员在类A中是用什么（public,protected,private）来修饰的。
+如果现在类A和类B都变成了类模板，那么能否让类模板B成为类模板A的友元类模板呢？
+1.1：让类模板的某个实例成为友元类
+1.2：让类模板成为友元类模板
+1.3：让类型模板参数成为友元类:C++11新标准中引入：如果传递进来的类型模板参数是一个类类型，则这个类类型可以成为当前类模板的友元类。
+a)代码行A2<CF> aobj1; 的效果是让CF类成为了A2<CF>类的友元类；
+b)于是，在CF类的成员函数中，可以直接访问aobj1这个A2<CF>类对象的data私有成员变量。
+如果传递给类模板A2的类型模板参数不是一个类类型，那么代码行friend T;就会被忽略。
+***/
+
+#include <iostream>
+
+//#include <boost/type_index.hpp>
+using namespace std;
+//#pragma warning(disable : 4996)
+
+namespace _nmsp1
+{
+	//template <typename U> class B;       //类模板B的声明
+
+	template <typename T>
+	class A
+	{
+		//friend class B<long>;            //不需要任何public,private等修饰符。
+		template<typename> friend class B; //类模板B作为类模板A的友元
+	private:
+		int data;
+	};
+
+	template <typename U>
+	class B
+	{
+	public:
+		void callBAF()                     //在类模板B中访问类模板A的私有成员
+		{
+			A<int> atmpobj;
+			atmpobj.data = 5;
+			cout << atmpobj.data << endl;
+		}
+	};
+}
+
+namespace _nmsp2
+{
+	template <typename T>
+	class A2
+	{
+		friend T;                       //类T成为类模板A2的友元
+		friend  class CF;
+
+	private:
+		int data;
+	};
+
+	class CF
+	{
+	public:
+		void callCFAF()
+		{
+			A2<CF> aobj1;                //让CF类成为了A2<CF>类的友元类
+			aobj1.data = 12;
+			cout << aobj1.data << endl;
+
+			A2<int> aobj2;               //因为CF类并不是A2<int>的友元类，自然不能在这里访问aobj2这个A2<int>类对象的data私有成员变量。
+			aobj2.data = 15;
+			cout << aobj2.data << endl;
+		}
+	};
+}
+
+int main()
+{
+	//测试nmsp1
+	//_nmsp1::B<long> bobj;
+	_nmsp1::B<int> bobj;
+	bobj.callBAF();
+
+	//测试nmsp2
+	_nmsp2::CF mycfobj;
+	mycfobj.callCFAF();
+
+	/*_nmsp2::A2<_nmsp2::CF> aobj1;
+	aobj1.data = 12;*/
+
+	return 0;
+}
---
+++ Templates/2/212.cpp
@@ -0,0 +1,101 @@
+﻿/***
+第三节 类模板中的友元
+
+(2)友元函数：函数模板可以被声明为友元函数
+(2.1)让函数模板的某个实例成为友元函数
+代码行_nmsp1::func(2, 3);，会实例化出 void func<int,int>(int,int){......}
+
+(2.2)友元模板
+将func函数模板（泛化版本）声明为Men类模板的友元模板之后，那么func函数模板的特化版本也会被看成是Men类模板的友元。
+编译器会把全特化的func函数模板看待成一个实例化过的函数模板。
+看待成了void func<int,double>(int val1, double val2)
+
+(2.3)在类模板中定义友元函数
+这种友元函数是能够被调用的，而且也只有在代码中调用了函数的时候，编译器才会实例化出这个函数。
+之所以这样定义友元函数，一般都是因为在该友元函数中会 用到这个类模板。
+ 这种友元函数的调用与调用普通函数函数，大家就把他当成普通函数来看待即可。
+func2在Men类模板被实例化时并不会被一并实例化出来，只有调用了func2的时候，才会被实例化出来。
+因为func2在类模板Men中，所以调用func2时，如果func2中的代码特别简单，则func2会被当成内联函数来处理。
+如果func2中的代码比较复杂，比如出现了for循环，那么func2很可能就不会被当做内联函数来处理。
+func2(mymen2); 可以被实例化出  void func2(class Men<double>&);
+func2(mymen3); 可以被实例化出  void func2(class Men<int>&);
+func2其实是个全局函数。
+***/
+
+#include <iostream>
+
+//#include <boost/type_index.hpp>
+using namespace std;
+//#pragma warning(disable : 4996) 
+
+namespace _nmsp1
+{
+	//函数模板func的声明
+	//template <typename U, typename V> void func(U val1, V val2);
+
+	//Men类模板
+	template <typename Z>
+	class Men
+	{
+		friend void func2(Men<Z>& tmpmen)
+		{
+			//for(int i= 0; i<1 ; ++i)
+				tmpmen.funcmen();
+		}
+
+		//friend void func<int, int>(int, int);  //<int,int>是两个模板实参
+		////friend void func<>(int, int);
+		////friend void func<int>(int, int);
+
+		////friend void func<float,int>(float, int);
+		//friend void func<>(float, int);
+
+		////friend void func<int, float>(int, float);
+		//friend void func<>(int, float);
+
+
+		//让函数模板func成为类模板Men的友元函数模板
+		template <typename U, typename V> friend void func(U val1, V val2);
+
+	private:
+		void funcmen() const
+		{
+			cout << "Men::funcmen被调用了" << endl;
+		}
+	};
+
+	template <typename U,typename V>
+	void func(U val1, V val2)
+	{
+		/*cout << "val1 = " << val1 << endl;
+		cout << "val2 = " << val2 << endl;*/
+		//Men mymen;
+		Men<int> mymen;
+		mymen.funcmen(); 
+	}
+
+	//func全特化版本
+	template <>
+	void func(int val1, double val2)
+	{
+		Men<int> mymen;
+		mymen.funcmen();
+	}
+
+}
+
+int main()
+{
+	//调用func的方法很多
+	//_nmsp1::func(2, 3);
+	//_nmsp1::func<float>(4.6f, 5); //第一个模板参数指定，第二个模板参数编译器自己推断
+	//_nmsp1::func<int, float>(4, 5.8f); //完全手工指定模板参数
+
+	_nmsp1::Men<double> mymen2;
+	func2(mymen2); //直接调用Men类模板中定义的友元函数func2
+
+	_nmsp1::Men<int> mymen3;
+	func2(mymen3);
+
+	return 0;
+}
